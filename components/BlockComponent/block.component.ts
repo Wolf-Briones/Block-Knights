3324070e2c071ac7d93f052dc4aa0b0b1c2ad625
Franki Briones <156685894+Wolf-Briones@users.noreply.github.com>
2024-10-18 18:52:38 -0500

    Create block.component.ts

---
+++ components/BlockComponent/block.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block',
+  templateUrl: './block.component.html'
+})
+export class BlockComponent {}
+/**Los bloques se colocan en el tablero cuando los jugadores hacen clic en las celdas vacías.*/
