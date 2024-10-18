22de979ec36dd2185ca146a65bc31eefcb4a817b
Franki Briones <156685894+Wolf-Briones@users.noreply.github.com>
2024-10-18 18:51:46 -0500

    Create knight.component.ts

---
+++ components/KnightComponent/knight.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-knight',
+  templateUrl: './knight.component.html'
+})
+export class KnightComponent {}
+/**4. KnightComponent (Caballero)
+Cada caballero es simplemente un marcador en el tablero. Aquí no se requiere un código complejo, ya que la interacción principal se gestiona en el tablero.*/
